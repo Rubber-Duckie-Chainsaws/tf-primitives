81f9a3a1dd325bd17ed114f57686c4a96de6898c
WigglesMcMuffin <teee.moose@gmail.com>
2024-06-24 05:12:16 -0400

    ECR lifecycle policy

--- aws/ecr/repo/main.tf
+++ aws/ecr/repo/main.tf
@@ -1,3 +1,11 @@
+locals {
+  policy = var.policy != "" ? var.policy : data.local_file.default_policy.content
+}
+
+data "local_file" "default_policy" {
+  filename = "${path.module}/policy.json"
+}
+
 resource "aws_ecr_repository" "this" {
   name = var.name
 
@@ -5,4 +13,10 @@ resource "aws_ecr_repository" "this" {
   tags = var.tags
 
   force_delete = true
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = local.policy
 }
